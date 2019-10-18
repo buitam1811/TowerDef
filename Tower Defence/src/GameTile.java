a87ae258f1c4fb6e6d64a1618161e90b76ea403e
tuuannthhannh <atonggiang0410@gmail.com>
2019-10-18 21:27:02 +0700

    change raw variable code to constant

--- Tower Defence/src/GameTile.java
+++ Tower Defence/src/GameTile.java
@@ -12,9 +12,9 @@ public class GameTile extends GameEntity{
 		}
 		graphic = (Graphics2D) bs.getDrawGraphics();
 		
-		graphic.clearRect(0, 0, 600,600 );
+		graphic.clearRect(0, 0, SCREEN_WIDTH*BLOCK_WIDTH,SCREEN_HEIGHT*BLOCK_HEIGHT );
 		
-		graphic.drawImage(this.image,0,0,600,600,null);
+		graphic.drawImage(this.image,0,0,SCREEN_WIDTH*BLOCK_WIDTH,SCREEN_HEIGHT*BLOCK_HEIGHT,null);
 		
 		graphic.dispose();
 		bs.show();
